23be0e2da44c848224ded99bba8fee1765fc2637
AndreaG <andreag19892@gmail.com>
2025-08-30 10:39:28 +0200

    creata API per la registrazione dell'utente che salva anche il primo blocco di voucher, migliorata anche la gestione degli errori

--- BACKEND/src/controllers/auth.controller.js
+++ BACKEND/src/controllers/auth.controller.js
@@ -5,31 +5,34 @@ const ErrorMessage = require ( '../errors/ErrorMessages' );
 const jwt = require ( 'jsonwebtoken' );
 const config = require ( '../../config' );
 const bcrypt = require ( 'bcrypt' );
-const {vaucherModel} = require ( '../models/voucher.model' );
+const {vaucherModel, voucherModel} = require ( '../models/voucher.model' );
 
 
 exports.register = async ( req, res ) => {
-    console.log ( "sono in registrazione" )
-    const error = validationResult ( req );
-    if ( !error.isEmpty () ) {
+    const errors = validationResult ( req );
+    if ( !errors.isEmpty () ) {
         return res.status ( 400 ).json ( {
+            success: false,
             errors: errors.array (),
             message: ErrorMessage.VALIDATION_ERR,
         } );
     }
-    const {name, email, password} = req.body;
+    const {name, email, password, nVoucher, valueOfVaucher} = req.body;
     try {
         if ( !name || !email || !password ) {
             return res.status ( 400 ).json ( {
-                errors: errors.array (),
+                success: false,
+                errors: [{msg: ErrorMessage.MISSING_FIELDS}],
                 message: ErrorMessage.MISSING_FIELDS,
             } );
         }
         const existingUser = await userModel.findOne ( {email: email} );
-        console.log (existingUser);
+        //console.log ( existingUser );
         if ( existingUser ) {
+            console.log ( "utente gia registrato" );
             return res.status ( 400 ).json ( {
-                errors: errors.array (),
+                success: false,
+                errors: [{msg: ErrorMessage.EMAIL_ALREADY_EXISTS}],
                 message: ErrorMessage.EMAIL_ALREADY_EXISTS,
             } );
         }
@@ -43,23 +46,39 @@ exports.register = async ( req, res ) => {
             password: hashedPassword,
         } );
         const savedUser = await newUser.save ();
+
+
+        //creo sulla tabella Voucher la riga relativa all'utente appena creato
+        const newVouchers = new voucherModel ( {
+            value: valueOfVaucher,
+            quantity: nVoucher,
+            userId: savedUser._id,
+        } );
+
+        //salvo il voucher:
+        const savedVouchers = await newVouchers.save ();
         return res.status ( 200 ).json ( {
+            success: true,
             message: 'User saved successfully.',
             user: savedUser,
-        } )
+            voucherData: savedVouchers,
+        } );
+
     } catch (err) {
         return res.status ( 500 ).json ( {
+            success: false,
             message: ErrorMessage.SERVER_ERROR, err
         } );
     }
 }
 
 exports.login = async ( req, res ) => {
-    console.log ("sono in login")
-    const error = validationResult ( req );
-    if ( !error.isEmpty () ) {
+    console.log ( "sono in login" )
+    const errors = validationResult ( req );
+    if ( !errors.isEmpty () ) {
         return res.status ( 400 ).json ( {
-            errors: errors.array (),
+            success: false,
+            errors: [{msg: ErrorMessage.VALIDATION_ERR}],
             message: ErrorMessage.VALIDATION_ERR,
         } );
     }
@@ -67,10 +86,11 @@ exports.login = async ( req, res ) => {
     try {
         //cerco l'utente tramite email
         const user = await userModel.findOne ( {email: email} );
-        console.log ("utente", user)
+        console.log ( "utente", user )
         if ( !user ) {
             return res.status ( 400 ).json ( {
-                errors: errors.array (),
+                success: false,
+                errors: [{msg: ErrorMessage.USER_NOT_FOUND}],
                 message: ErrorMessage.USER_NOT_FOUND
             } );
         }
@@ -78,13 +98,15 @@ exports.login = async ( req, res ) => {
         const validPass = await bcrypt.compare ( password, user.password );
         if ( !validPass ) {
             return res.status ( 400 ).json ( {
-                errors: errors.array (),
+                success: false,
+                errors: [{msg: ErrorMessage.INVALID_CREDENTIALS}],
                 message: ErrorMessage.INVALID_CREDENTIALS
             } )
         }
         //genero il token per l'accesso:
         const token = jwt.sign ( {id: user.id}, config.jwtSecret, {expiresIn: '1h'} );
         return res.status ( 200 ).json ( {
+            success: true,
             token: token,
             message: "login riuscito",
             userData: {
@@ -95,6 +117,7 @@ exports.login = async ( req, res ) => {
         } )
     } catch (err) {
         res.status ( 500 ).json ( {
+            success: false,
             message: ErrorMessage.SERVER_ERROR, err
         } )
     }
