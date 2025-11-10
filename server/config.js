a5b5ff993a382434e66eb2c50398ba9b8ba9f136
AndriG19892 <68709776+AndriG19892@users.noreply.github.com>
2025-11-10 18:55:13 +0100

    Update database connection string to use MONGO_URI

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 const config = {
     port: process.env.PORT || 8080,
     jwtSecret: process.env.JWT_SECRET,
-    connectionString: process.env.CONNECTION_STRING_DB,
+    connectionString: process.env.MONGO_URI,
 }
 
 module.exports = config
