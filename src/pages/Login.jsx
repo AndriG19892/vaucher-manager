8347308a5f631107aa41edc9877c873a8c8f81a9
AndriG19892 <68709776+AndriG19892@users.noreply.github.com>
2025-11-11 09:11:00 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,6 +21,7 @@ const Login = () => {
                 email,
                 password,
             } );
+console.log("risposta:",responseFromServer);
             if ( responseFromServer.data.success && responseFromServer.data.token ) {
                 SaveInLocalStorage ( 'token', responseFromServer.data.token );
                 SaveInLocalStorage ( 'userId', responseFromServer.data.userData.id );
