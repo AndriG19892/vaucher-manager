d09020b47a808b07926e5a8d5bce58398b93cd74
AndriG19892 <68709776+AndriG19892@users.noreply.github.com>
2026-02-05 15:17:40 +0100

    Add jwtSecret and allowedOrigins to config

--- server/config.js
+++ server/config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 module.exports = {
     port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET,
     connectionString: process.env.MONGO_URI,
     // Aggiungi questa riga:
     allowedOrigins: process.env.ALLOWED_ORIGINS || 'http://localhost:5173' 
