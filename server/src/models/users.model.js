a95589d1f0729a5a79ec6983e604aa3e83916773
AndreaG <andreag19892@gmail.com>
2026-02-12 17:43:21 +0100

    modificato userModel con funzione pre per criptare la password alla registrazione di un nuovo utente

--- server/src/models/users.model.js
+++ server/src/models/users.model.js
@@ -1,31 +1,30 @@
 const uuid = require ( 'uuid' );
 const mongoose = require ( 'mongoose' );
+const bcrypt = require ( 'bcrypt' );
 
 const usersSchema = new mongoose.Schema ( {
-    id: {
-        type: String,
-        required: true,
-        unique: true,
-        default: () => uuid.v7 ()
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     password: {
         type: String,
         required: true,
+        select: false
+    },
+}, {timestamps: true} );
+
+
+//Middleware pre-save
+usersSchema.pre ( 'save', async function ( next ) {
+    //criptiamo la password solo se è sata modificata (o è nuova)
+    if ( !this.isModified ( 'password' ) ) return next ();
+    try {
+        const salt = await bcrypt.genSalt ( 10 );
+        this.password = await bcrypt.hash ( this.password, salt );
+        next ();
+    } catch (err) {
+        next ( err );
     }
 } );
+const userModel = mongoose.model ( 'Users', usersSchema, 'User' );
 
-const userModel = mongoose.model( 'Users', usersSchema, 'User' );
-
-module.exports = {
-    userModel,
-    usersSchema
-};
+module.exports = {userModel};
